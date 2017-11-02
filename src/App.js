f4b002a5e163acca1a71fb3be87ce4123dbb7ce5
Kenneth <k.aasan@sportradar.com>
2017-11-02 12:50:50 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
 
-class App extends Component {
+import React, { Component } from 'react';
+
+class App extends Component<{}> {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
